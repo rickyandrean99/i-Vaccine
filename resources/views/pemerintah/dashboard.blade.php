795723bcbe0277ff7ca95c9d243092408e56a10c
rickyandrean99 <ricky.ralph09@gmail.com>
2021-07-28 03:04:12 +0700

    Daftar Vaksin Done

--- resources/views/pemerintah/dashboard.blade.php
+++ resources/views/pemerintah/dashboard.blade.php
@@ -4,24 +4,60 @@
     <div class="row">
         <div class="col-12 mt-5">
             <div class="text-center mb-5 mb-md-5 mt-1 mt-md-4">
-                <p><b>Grafik Vaksinasi</b></p>
+                <div class="h1 text-center mb-5 mb-md-5 mt-1 mt-md-4 header-section">Grafik Vaksinasi</div>
                 <img src="{{ asset('assets/img/covid.png') }}" alt="gambar grafik" class="img-fluid ">
 
-                <div class="row">
-                    <div class="card ms-5 me-5" style="width: 25%;">
-                        <div class="card-body">
-                            <h5 class="card-title">Vaksin Pertama</h5>
-                            <p class="card-text">34.173.832</p>
+                <div class="row justify-content-center">
+                <div class="h1 text-center mb-5 mb-md-5 mt-1 mt-md-4 header-section">Data</div>
+
+                    <div class="col-6 mb-5">
+                        <div class="card ms-5 me-5 w-100">
+                            <div class="card-body">
+                                <h5 class="card-title">Vaksin Pertama</h5>
+                                <p class="card-text">34.173.832</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-6 mb-5">
+                        <div class="card ms-5 me-5 w-100">
+                            <div class="card-body">
+                                <h5 class="card-title">Vaksin Kedua</h5>
+                                <p class="card-text">13.584.190</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-6">
+                        <div class="card ms-5 me-5 w-100">
+                            <div class="card-body">
+                                <h5 class="card-title">Stok Tersedia</h5>
+                                <p class="card-text">131.451.247</p>
+                            </div>
                         </div>
                     </div>
 
-                    <div class="card ms-5 me-5" style="width: 25%;">
-                        <div class="card-body">
-                            <h5 class="card-title">Vaksin Kedua</h5>
-                            <p class="card-text">13.584.190</p>
+                    <div class="col-6">
+                        <div class="card ms-5 me-5 w-100">
+                            <div class="card-body">
+                                <h5 class="card-title">Penyelenggara</h5>
+                                <p class="card-text">47.193</p>
+                            </div>
                         </div>
                     </div>
                 </div>
+
+                <section id="peta-sebaran">
+                    <div class="container daftar-vaksin-wrapper rubik-medium pt-5 pb-5">
+                        <div class="h1 text-center mb-5 mb-md-5 mt-1 mt-md-4 header-section">Peta Sebaran Vaksinasi COVID-19</div>
+
+                        <div class="row mb-4 justify-content-center">
+                            <div class="col-11 col-md-12 bg-primary text-white text-center h4 d-flex align-items-center justify-content-center" style="min-height: 500px">
+                                Google Maps API + Python Matplotlib
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
         </div>
     </div>
--- resources/views/pemerintah/penyelenggara.blade.php
+++ resources/views/pemerintah/penyelenggara.blade.php
@@ -1,7 +1,62 @@
 @extends('layout.pemerintah')
 
 @section('content')
+    <div class="row">
+        <div class="col-12 mt-5">
+            <div class="text-center mb-5 mb-md-5 mt-1 mt-md-4">
+                <div class="h1 text-center mb-5 mb-md-5 mt-1 mt-md-4 header-section">Data Penyelenggara</div>
+                <form action="" method="post">
+                    <select name="" id="" class="form-select">
+                        <option value="">-- Tampilkan Perdaerah --</option>
+                        <option value="">Denpasar</option>
+                        <option value="">Badung</option>
+                        <option value="">Singaraja</option>
+                        <option value="">Negara</option>
+                        <option value="">Karangasem</option>
+                    </select>
+                </form>
+                <div class="text-end mb-5 mb-md-5 mt-1 mt-md-4 header-section">
+                    Nama Penyelenggara:
+                    <form action="" method="post" class="d-inline">
+                        <input type="text" placeholder="Nama Penyelenggara" class="w-25 p-2">
+                    </form>
+                </div>
 
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col" style="min-width: 70px; width: 70px">No</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">Alamat</th>
+                            <th scope="col" style="width: 150px;"></th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Jalanan</td>
+                            <td><a href="">Detail</a></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Jalanin</td>
+                            <td><a href="">Detail</a></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Matthew</td>
+                            <td>Jalanon</td>
+                            <td><a href="">Detail</a></td>
+                        </tr>
+                    </tbody>
+                    </table>
+            </div>
+
+        </div>
+    </div>
     <script>
         $('#nav-penyelenggara').addClass('active');
     </script>
